1d15066043ceeb2b1f2a77cf713be6c3c0e1ca94
theo-kirby <theokirby15@gmail.com>
2022-03-14 10:36:22 -0400

    scripts folder

---
+++ tGo/Assets/scripts/Group.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Group : MonoBehaviour
+{
+    public int groupNumber;
+    public string color;
+    public int stoneCount;
+    public Stone[] stonesInGroup;
+    public int liberties = 0;
+    
+
+}
